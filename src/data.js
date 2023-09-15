dd75412a19315337ca4b51f41654c9c59ca2bd14
DevitoDbug <Davidochiengy@gmail.com>
2023-09-15 11:51:23 +0300

    Fix a bug

---
+++ src/data.js
@@ -0,0 +1,259 @@
+export const movies = [
+  {
+  id:1,
+  title:"One Piecce",
+  image:"/images/action1.webp",
+  about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+  year: "2017 Limited Series HD" ,
+  cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+  genre:"Action, Drama, Thriller",
+  video:"/images/ONE PIECE _ Official Trailer _ Netflix_HIGH.mp4",
+  
+  
+  },
+
+
+{
+    id:2,
+    title:"Movie 2",
+    image:"/images/action2.webp",
+    about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+    year: "2017 Limited Series HD" ,
+    cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+    genre:"Action, Drama, Thriller",
+    video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+  
+
+    },  
+   
+{
+      id:3,
+      title:"Movie 3",
+      image:"/images/action3.webp",
+      about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda"  ,
+      year: "2017 Limited Series HD" ,
+      cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+      genre:"Action, Drama, Thriller",
+      video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+    
+      },
+
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/action4.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda"  ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+      
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/action5.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda " ,
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+          video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+        
+          },
+          
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/action6.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda"  ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+      
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/action8.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda " ,
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+            video:"/images/Extraction _ Official Trailer _ Screenplay by JOE RUSSO Directed by SAM HARG_HIGH.mp4",
+        
+          },
+
+];
+
+export const Series = [
+  {
+  id:1,
+  title:"The Flash",
+  image:"/images/series1.webp",
+  about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda"  ,
+  year: "2017 Limited Series HD" ,
+  cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+  genre:"Action, Drama, Thriller",
+  video:"/images/The Flash – Official Trailer_HIGH.mp4",
+  },
+
+
+{
+    id:2,
+    title:"Witcher",
+    image:"/images/series2.webp",
+    about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+    year: "2017 Limited Series HD" ,
+    cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+    genre:"Action, Drama, Thriller",
+    video:"/images/THE WITCHER _ MAIN TRAILER _ NETFLIX_HIGH.mp4",
+    },  
+   
+{
+      id:3,
+      title:"Movie 3",
+      image:"/images/series3.webp",
+      about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda"  ,
+      year: "2017 Limited Series HD" ,
+      cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+      genre:"Action, Drama, Thriller",
+      video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+      },
+
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/series4.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/series6.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda ",
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+          video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+          },
+          
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/series7.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/series1.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda ",
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+          video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+          },
+
+];
+
+
+
+export const Teens = [
+  {
+  id:1,
+  title:"Titans",
+  image:"/images/teen.webp",
+  about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+  video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+  year: "2017 Limited Series HD" ,
+  cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+  genre:"Action, Drama, Thriller"
+ 
+  },
+
+
+{
+    id:2,
+    title:"Movie 2",
+    image:"/images/teen2.webp",
+    about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+    video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+    year: "2017 Limited Series HD" ,
+    cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+    genre:"Action, Drama, Thriller"
+   
+    },  
+   
+{
+      id:3,
+      title:"Movie 3",
+      image:"/images/teen5.webp",
+      about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+      year: "2017 Limited Series HD" ,
+  cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+  genre:"Action, Drama, Thriller",
+ 
+      video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+      },
+
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/teen7.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/thumbbig-1325035.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda ",
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+          video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+          },
+          
+      {
+        id:4,
+        title:"Movie 4",
+        image:"/images/series7.webp",
+        about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda" ,
+        year: "2017 Limited Series HD" ,
+        cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+        genre:"Action, Drama, Thriller",
+        video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+        },
+
+        {
+          id:5,
+          title:"Movie 5",
+          image:"/images/series1.webp",
+          about:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit architecto nulla autem libero temporibus facilis porro assumenda ",
+          year: "2017 Limited Series HD" ,
+          cast:"Chris Hemsworth, Chris Pratt ,Angelina Jolie ,John Cena ,Dwayne Johnson ,Kevin Hart ,Ice Cube ",
+          genre:"Action, Drama, Thriller",
+          video:"/images/Titans _ Official Trailer _ Netflix_HIGH.mp4",
+          },
+
+
+
+
+];
