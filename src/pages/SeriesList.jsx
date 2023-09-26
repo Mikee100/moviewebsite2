e7d15a53460320eaaa379b8f5db97c8d26c5cce0
Mike <mikekariuki10028@gmail.com>
2023-09-26 23:08:46 +0300

    worked on modal on SeriesList

--- src/pages/SeriesList.jsx
+++ src/pages/SeriesList.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import Series from "../data/series";
 import { FaPlay } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
-
+import "./home.css"
 import "../styles/movielist.css";
 import { FaInfoCircle } from "react-icons/fa";
+
+import { FaTimes } from "react-icons/fa";
+import { FaRegThumbsUp } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+
 
 //const getMovieById = (id) => {
 //return movies.find((action) => action.id === id);
@@ -15,9 +20,23 @@ export default function SeriesList() {
 
   const [modal, setModal] = useState(false);
 
+
+  const [modal1, setModal1] = useState(false);
+  const [popupcontent, setpopupcontent] = useState([]);
+
+  const changecontent = (teens) => {
+    setpopupcontent([teens]);
+    setModal(!modal);
+  };
+
   const toggleModal = () => {
     setModal(!modal);
   };
+
+  const toggleModal1 = () => {
+    setModal1(!modal1);
+  };
+
   return (
     <div>
       <div className="container_movies">
@@ -45,8 +64,8 @@ export default function SeriesList() {
           <p>Movie not found</p>
         )}
         <h2 className="movie_genre">Series</h2>
-        <div className="subgenre" onClick={toggleModal}>
-          {modal && (
+        <div className="subgenre" onClick={toggleModal1}>
+          {modal1 && (
             <section>
               <option>Action</option>
               <option>Comedy</option>
@@ -84,7 +103,9 @@ export default function SeriesList() {
           <h2>Popular on Netflix</h2>
           <div className="list_movie_arrange">
             {Series.map((movie) => (
-              <div className="list_movies" key={movie.id}>
+              <div className="list_movies" key={movie.id} 
+              onClick={() => changecontent(movie)}>
+                  
                 <img
                   className="movie-image"
                   src={movie.image}
@@ -99,7 +120,9 @@ export default function SeriesList() {
           <div className="action_arrange">
             <h2>Top 10 Movies in Kenya </h2>
             {Series.map((movie) => (
-              <div className="list_movies" key={movie.id}>
+              <div className="list_movies" key={movie.id}
+              onClick={() => changecontent(movie)}>
+                
                 <img
                   className="movie-image"
                   src={movie.image}
@@ -109,7 +132,59 @@ export default function SeriesList() {
             ))}
           </div>
         </div>
+
+
+        {modal && (
+          <div className="modal" onClick={toggleModal}>
+            {popupcontent.map((pop) => {
+              return (
+                <div className="popup_movie" key={pop.id}>
+                  <video
+                    controls
+                    width="100%"
+                    id="video"
+                    className="pop_video"
+                    autoPlay
+                    muted
+                  >
+                    <source src={pop.video} type="video/mp4" />
+                    Sorry, your browser doesn't support embedded videos.
+                  </video>
+
+                  <p className="pop_title">{pop.title}</p>
+                  <p className="pop_about">{pop.about}</p>
+                  <p className="pop_year">{pop.year}</p>
+                  <p className="pop_cast">
+                    {" "}
+                    <p>Cast:</p> {pop.cast}
+                  </p>
+                  <p className="pop_genre">
+                    {" "}
+                    <p>Genre:</p> {pop.genre}
+                  </p>
+
+                  <p className="watchnow">Watch the Movie</p>
+                  <button className="pop_btn_play" onClick={pop.video}>
+                    {" "}
+                    <FaPlay className="faplay" /> <p>Play</p>{" "}
+                  </button>
+                  <FaPlus className="addtolist" />
+                  <FaRegThumbsUp className="thumbsup" />
+                  <button onClick={changecontent} className="cross">
+                    <FaTimes />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+
+
       </div>
+
+
+
     </div>
   );
 }
